67f4e885f5879637469ea094ce3cd13e7782d22b
Mordechaie <mordynys@gmail.com>
2024-09-23 18:38:56 -0400

    1-4 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,7 @@
-const reverseString = function() {
-
-};
+// Create a for loop. letter in reversestring place in new string.
+const reverseString = function (string) {
+    return string.split("").reverse().join("");
+  };
 
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    // create a new empty array
+  let myList = [1, 2, 3, 4];
+  myList.splice(1, 2);
+  return myList;
+    
 };
 
 // Do not edit below this line
