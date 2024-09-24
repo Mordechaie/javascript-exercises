630a3c987b67540602b2ac11e754e9bbd1e0b02b
Mordechaie <mordynys@gmail.com>
2024-09-23 23:37:10 -0400

    5 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    // sum variable
+    let sum = 0;
 
+    // for loop to loop through num1 to num2
+    for (let i = num1; i <= num2; i ++){
+        sum += i
+    }
+    return counter.sum()
 };
 
 // Do not edit below this line
